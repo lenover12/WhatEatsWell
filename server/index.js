7cad43c55d51a844e0905e4988b9d24d59718059
lenover12 <lennymcdonald247@hotmail.com>
2024-03-25 18:01:40 +1100

    Add comments & Update package.json for ava tests

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
-// Node.js as the runtime environment and ES Modules for module management
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
 
+// load environment variables from .env file
 dotenv.config();
+
+// alias for the MongoDB client
 const MongoClient = mongodb.MongoClient;
 
+// define the port to listen on
 const port = process.env.PORT || 8000;
 
+// connect to mongoDB database
 MongoClient.connect(process.env.ACCOUNTS_DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   wtimeout: 2500,
 })
+  // log connection errors and close connection
   .catch((err) => {
     console.error(err.stack);
     process.exit(1);
   })
+  // start express server and listen
   .then(async () => {
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
