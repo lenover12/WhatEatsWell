2a750cd55c90cf804ad96643e3b3a2fee28ca988
lenover12 <lennymcdonald247@hotmail.com>
2024-05-27 19:06:31 +1000

    Login & Register initial state logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+
 
 export default function Login() {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+  
+  const loginUser = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <div>
+    <form onSubmit={loginUser}>
+      <label>Email</label>
+      <input type="text" placeholder='enter email..' value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
+      <label>Password</label>
+      <input type="text" placeholder='enter password..' value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
+      <button type='submit'>Login</button>
+    </form>
+  </div>
   )
 }
